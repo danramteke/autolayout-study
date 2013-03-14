f967913533e279d7645e505e3a7238a3df1d658b
Daniel R <danramteke@gmail.com>
2013-03-13 20:32:36 -0400

    Initial Commit

---
+++ Autolayout StudyTests/Autolayout_StudyTests.h
@@ -0,0 +1,13 @@
+//
+//  Autolayout_StudyTests.h
+//  Autolayout StudyTests
+//
+//  Created by Daniel R on 3/13/13.
+//  Copyright (c) 2013 Daniel R. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Autolayout_StudyTests : SenTestCase
+
+@end
---
+++ Autolayout StudyTests/Autolayout_StudyTests.m
@@ -0,0 +1,32 @@
+//
+//  Autolayout_StudyTests.m
+//  Autolayout StudyTests
+//
+//  Created by Daniel R on 3/13/13.
+//  Copyright (c) 2013 Daniel R. All rights reserved.
+//
+
+#import "Autolayout_StudyTests.h"
+
+@implementation Autolayout_StudyTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Autolayout StudyTests");
+}
+
+@end
